cf1484bb5b36fc82bbd8e385811a51c52b051d08
Mahmoud-hamdii <ma7moud.7oda97@gmail.com>
2024-06-30 16:07:56 +0300

    change the config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  base: "/todo-project/",
+  base: "/todo-project",
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
 });
