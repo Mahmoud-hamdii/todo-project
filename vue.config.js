cb60734108ba9116983722efd609cbe95af084b4
Mahmoud-hamdii <ma7moud.7oda97@gmail.com>
2024-06-30 16:21:08 +0300

    change the cinfig

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  base: "/",
+  publicPath: "/",
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
 });
