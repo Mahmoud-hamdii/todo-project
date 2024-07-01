ad9057b78c19da035ed76bb3fbd1e77f697f0d17
Mahmoud-hamdii <115195320+Mahmoud-hamdii@users.noreply.github.com>
2024-07-01 16:50:27 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/",
+  base: "/",
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
 });
